20726cde07836255405380b74c8e3f67bf27b140
David <brushdm@gmail.com>
2022-06-08 22:17:48 -0400

    First commit for Lab 4.4. Nearly finished

---
+++ Blockbuster/src/BlockbusterJUnitTesting.java
@@ -0,0 +1,138 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class BlockbusterJUnitTesting {
+
+	@Test
+	void testDVDInstanceCreation() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		DVD dvd1 = new DVD("Planet of the Apes", 183, apeScenes);
+		
+		assertEquals(dvd1.toString(), new DVD("Planet of the Apes", 183, apeScenes).toString());
+	}
+	
+	@Test
+	void testDVDTitleSetter() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		DVD dvd1 = new DVD("Planet of the Apes", 183, apeScenes);
+		dvd1.setTitle("PoA");
+		
+		assertEquals("PoA", dvd1.getTitle());
+	}
+	
+	@Test
+	void testDVDRuntimeSetter() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		DVD dvd1 = new DVD("Planet of the Apes", 183, apeScenes);
+		dvd1.setRunTime(190);
+		
+		assertEquals(190, dvd1.getRunTime());
+	}
+	
+	@Test
+	void testVHSInstanceCreation() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		VHS vhs1 = new VHS("Planet of the Apes", 183, apeScenes);
+		
+		assertEquals(vhs1.toString(), new VHS("Planet of the Apes", 183, apeScenes).toString());
+	}
+	
+	@Test
+	void testVHSTitleSetter() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		VHS vhs1 = new VHS("Planet of the Apes", 183, apeScenes);
+		vhs1.setTitle("PoA");
+		
+		assertEquals("PoA", vhs1.getTitle());
+	}
+	
+	@Test
+	void testVHSRuntimeSetter() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		VHS vhs1 = new VHS("Planet of the Apes", 183, apeScenes);
+		vhs1.setRunTime(190);
+		
+		assertEquals(190, vhs1.getRunTime());
+	}
+	
+	@Test
+	void testVHSCurrentTimeSetPoint() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		VHS vhs1 = new VHS("Planet of the Apes", 183, apeScenes);
+		
+		assertEquals(0, vhs1.getCurrentTime());
+	}
+	
+	@Test
+	void testVHSPlayMethod() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		VHS vhs1 = new VHS("Planet of the Apes", 183, apeScenes);
+		vhs1.play(apeScenes);
+		vhs1.play(apeScenes);
+		vhs1.play(apeScenes);
+		
+		assertEquals(3, vhs1.getCurrentTime());
+	}
+	
+	
+	@Test
+	void testVHSRewindMethod() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		VHS vhs1 = new VHS("Planet of the Apes", 183, apeScenes);
+		vhs1.setCurrentTime(3);
+		vhs1.rewind();
+		
+		
+		assertEquals(0, vhs1.getCurrentTime());
+	}
+	
+	@Test
+	void testVHSRewindMethod2() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		VHS vhs1 = new VHS("Planet of the Apes", 183, apeScenes);
+		vhs1.play(apeScenes);
+		vhs1.play(apeScenes);
+		vhs1.play(apeScenes);
+		vhs1.play(apeScenes);
+		
+		assertEquals(1, vhs1.getCurrentTime());
+	}
+	
+	@Test
+	void testMoviePlayMethod() {
+		
+		ArrayList<String> apeScenes = new ArrayList<>(Arrays.asList("Apes are poor", "Apes take over Planet", "Holy sh@t, the statue of liberty is an ape??"));
+		
+		Movie movie1 = new Movie("Planet of the Apes", 183, apeScenes, 0);
+		movie1.play(0);
+		movie1.play(1);
+		
+		assertEquals(2, movie1.getCurrentTime());
+	}
+	
+
+}
---
+++ Blockbuster/src/Movie.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie implements Play {
+	
+	String title;
+	int runTime;
+	ArrayList<String> scenes;
+	int iPlay;
+	int currentTime;
+
+	public Movie(String title, int runTime, ArrayList<String> scenes, int iPlay) {
+		super();
+		this.title = title;
+		this.runTime = runTime;
+		this.scenes = scenes;
+		this.iPlay = iPlay;
+	}
+	
+	public int getCurrentTime() {
+		return currentTime;
+	}
+
+	public void setCurrentTime(int currentTime) {
+		this.currentTime = currentTime;
+	}
+	
+	public int getiPlay() {
+		return iPlay;
+	}
+
+	public void setiPlay(int iPlay) {
+		this.iPlay = iPlay;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getRunTime() {
+		return runTime;
+	}
+
+	public void setRunTime(int runTime) {
+		this.runTime = runTime;
+	}
+
+	public ArrayList<String> getScenes() {
+		return scenes;
+	}
+
+	public void setScenes(ArrayList<String> scenes) {
+		this.scenes = scenes;
+	}
+	
+	public void printInfo() {
+		System.out.println("Movie title: " + title + "Runetime: " + runTime);
+	}
+	
+	public void printScenes() {
+		for(int i = 0; i < scenes.size(); i++) {
+			System.out.println("Scene " + i + ": " + scenes.get(i));		
+		}
+	}
+	
+	public void play(int iPlay) {
+		play(scenes);
+	}
+
+	@Override
+	public void play(List<String> scenes) {
+			System.out.println("Scene " + iPlay + ": " + scenes.get(iPlay));
+			currentTime++;
+	}
+ 
+}
